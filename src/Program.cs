0cb5ecd4ee00aa4fcfbe377beb802109222ff0ee
long.liang <long.liang@shopee.com>
2025-09-16 11:06:00 +0800

    feat: support batch

--- src/Program.cs
+++ src/Program.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
 using Windows.Storage;
-using Windows.Storage.Streams;
 using System.Text.Json;
 using Windows.Media.Ocr;
 using Windows.Globalization;
 using System.CommandLine;
-using System.CommandLine.Completions;
 
-
-var fileOption = new Option<string>(
-    name: "--file",
-    description: "The file to read and display on the console."
-) {
-    IsRequired = true
+var fileOption = new Option<string[]>(
+    name: "--files",
+    description: "The files to read and display on the console."
+)
+{
+    IsRequired = true,
+    AllowMultipleArgumentsPerToken = true
 };
 var languageOption = new Option<string>(
     name: "--language",
@@ -36,37 +33,39 @@ var rootCommand = new RootCommand("Start an OCR analysis using Windows local Ocr
     languageOption,
     modeOption
 };
-rootCommand.SetHandler(Handler, fileOption, languageOption, modeOption);
+rootCommand.SetHandler(async (files, lang, mode) => await Handler(files, lang, mode), fileOption, languageOption, modeOption);
 
 return await rootCommand.InvokeAsync(args);
 
-
-static async Task Handler(string filepath, string language, OcrOutputMode mode)
+static async Task Handler(string[] filepaths, string language, OcrOutputMode mode)
 {
-    var result = await RecognizeAsync(filepath, language);
+    var results = await RecognizeBatchFromPath(filepaths, language);
     var txt = "";
 
     if (mode == OcrOutputMode.json)
     {
-        txt = JsonSerializer.Serialize(result);
+        txt = JsonSerializer.Serialize(results);
     }
     else if (mode == OcrOutputMode.text)
     {
         var sb = new StringBuilder();
-        foreach (var l in result.Lines)
+        foreach (var result in results)
         {
-            var line = new StringBuilder();
-
-            foreach (var word in l.Words)
+            foreach (var l in result.result.Lines)
             {
-                line.Append(word.Text);
-                if (!language.Contains("zh"))
+                var line = new StringBuilder();
+
+                foreach (var word in l.Words)
                 {
-                    line.Append(" ");
+                    line.Append(word.Text);
+                    if (!language.Contains("zh"))
+                    {
+                        line.Append(" ");
+                    }
                 }
+                sb.Append(line);
+                sb.Append(Environment.NewLine);
             }
-            sb.Append(line);
-            sb.Append(Environment.NewLine);
         }
         txt = sb.ToString();
     }
@@ -74,38 +73,49 @@ static async Task Handler(string filepath, string language, OcrOutputMode mode)
     Console.WriteLine(txt);
 }
 
-
-static async Task<OcrResult> RecognizeAsync(string filepath, string language)
+static async Task<List<CustomOcrResult>> RecognizeBatchFromPath(string[] filepaths, string language)
 {
-    var path = Path.GetFullPath(filepath);
-    var storageFile = await StorageFile.GetFileFromPathAsync(path);
-    using var randomAccessStream = await storageFile.OpenReadAsync();
-    var decoder = await BitmapDecoder.CreateAsync(randomAccessStream);
-    using var softwareBitmap = await decoder.GetSoftwareBitmapAsync(
-        BitmapPixelFormat.Bgra8,
-        BitmapAlphaMode.Premultiplied
-    );
-    var lang = new Language(language);
-    if (OcrEngine.IsLanguageSupported(lang))
+    var engine = IsLanguageSupported(language) ? OcrEngine.TryCreateFromLanguage(new Language(language)) : OcrEngine.TryCreateFromUserProfileLanguages();
+    if (engine == null) { throw new Exception($"Could not instanciate OcrEngine for language {language}."); }
+
+    var results = new List<CustomOcrResult>();
+    foreach (var filepath in filepaths)
     {
-        var engine = OcrEngine.TryCreateFromLanguage(lang);
-        if (engine != null)
+        var path = Path.GetFullPath(filepath);
+        var storageFile = await StorageFile.GetFileFromPathAsync(path);
+        using var randomAccessStream = await storageFile.OpenReadAsync();
+        var decoder = await BitmapDecoder.CreateAsync(randomAccessStream);
+        using var softwareBitmap = await decoder.GetSoftwareBitmapAsync(
+            BitmapPixelFormat.Bgra8,
+            BitmapAlphaMode.Premultiplied
+        );
+
+        var result = new CustomOcrResult
         {
-            return await engine.RecognizeAsync(softwareBitmap);
-        }
-        else
-        {
-            throw new Exception($"Could not instanciate OcrEngine for language {language}.");
-        }
+            language = engine.RecognizerLanguage.LanguageTag,
+            result = await engine.RecognizeAsync(softwareBitmap)
+        };
+
+        results.Add(result);
     }
-    else
-    {
-        throw new Exception($"Language {language} is not supported");
-    }
+
+    return results;
 }
+
+static bool IsLanguageSupported(string language)
+{
+    return OcrEngine.IsLanguageSupported(new Language(language));
+}
+
 
 enum OcrOutputMode
 {
     json,
     text
+}
+
+class CustomOcrResult
+{
+    public string language { get; set; }
+    public OcrResult result { get; set; }
 }
